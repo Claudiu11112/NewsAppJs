e7df22a5be7751e33fba3b364899d412ed110fdb
Claudiu <mhvsl@yahoo.com>
2022-04-30 10:04:46 +0300

    Sat 30:04:2022 - 10:04:36 AM

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let arr = [];
 
 function loadNews() {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((json) => (arr = [...json]));
-  // document.getElementById("card").innerHTML = "test1";
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => (arr = [...json]));
+    // document.getElementById("card").innerHTML = "test1";
 
-  setTimeout(() => {
-    document.getElementById("cardNew").innerHTML = arr
-      .map((user, i) => {
-        return `<div class="card" >
+    setTimeout(() => {
+        document.getElementById("cardNew").innerHTML = arr
+            .map((user, i) => {
+                return `<div class="card" >
             <img class="img" onclick="txtAlert()" alt="news" id="${user.id}"
             src="https://source.unsplash.com/collection/${user.id}/320x320"/>
 
@@ -18,18 +18,18 @@ function loadNews() {
             <p class="bodytxt">${user.body + " " + user.id}</p>
             </div>
             </div>`;
-      })
-      .join(" "); // fara join pune virgula intre carduri
-  }, 300);
+            })
+            .join(" "); // fara join pune virgula intre carduri
+    }, 300);
 }
 
 function txtAlert() {
-  let id;
-  window.onclick = (e) => {
-    id = e.target.id;
-    // alert(arr[id].title);
+    let id;
+    window.onclick = (e) => {
+        id = e.target.id;
+        // alert(arr[id].title);
 
-    document.getElementById("cardNew").innerHTML = `<div class="card2" >
+        document.getElementById("cardNew").innerHTML = `<div class="card2" >
     <img class="img" onclick="loadNews()" alt="news" id="${id}"
     src="https://source.unsplash.com/collection/${id}/1120x700"/>
     
@@ -38,5 +38,5 @@ function txtAlert() {
     <p class="bodytxt">${arr[id].body + " " + id}</p>
     </div>
     </div>`;
-  };
+    };
 }
